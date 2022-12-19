747060da174a319c9a04f80337722aadac9bf5ba
Yuna <85014628+vipwhy12@users.noreply.github.com>
2022-12-20 03:54:24 +0900

    🎉Begin a project

---
+++ src/test/java/hello/hellospring/sevice/MemberServiceTest.java
@@ -0,0 +1,35 @@
+package hello.hellospring.sevice;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
